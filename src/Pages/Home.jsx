63e5b024120a064fef7d8c810b5150a746c46d49
MostakemHossain <mostakememon123@gmail.com>
2023-11-20 22:28:40 +0600

    Banner section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import Banner from "../components/Banner";
 
 
 const Home = () => {
+  const [query,setQuery]=useState("");
+  const handleInputChange=(e)=>{
+      setQuery(e.target.value);
+    
+  }
+  console.log(query);
   return (
-    <div>Home</div>
+    <div>
+      <Banner query={query} handleInputChange={handleInputChange}/>
+    </div>
   )
 }
 
